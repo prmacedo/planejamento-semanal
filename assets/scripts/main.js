bd3de41eed3aec2eca0ee331aaec25773f6d567a
prmacedo <p.ricardomacedo@hotmail.com>
2022-10-01 09:10:32 -0300

    refactor code, create Clock class

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,31 +1,6 @@
-// Clock
-function setCurrentDate() {
-  const clockDate = document.querySelector('.js-clock__date');
+import { Clock } from "./Clock.js";
 
-  const dateOptions = { day: 'numeric', month: 'long', year: 'numeric' };
-  const today = new Date().toLocaleDateString('pt-br', dateOptions);
-
-  clockDate.textContent = today;
-}
-
-setCurrentDate();
-
-function setCurrentHour() {
-  const clockHour = document.querySelector('.js-clock__hour');
-
-  const now = new Date();
-  const hour = now.getHours() + ':' + now.getMinutes().toString().padStart(2, '00');
-
-  clockHour.textContent = hour;
-}
-
-function updateClock() {
-  setCurrentHour();
-
-  setInterval(setCurrentHour, 1000 * 60);
-}
-
-updateClock();
+new Clock();
 
 // Board
 const activityList = [
