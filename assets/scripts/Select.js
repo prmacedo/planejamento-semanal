b9a505fa58ae7074829ae27207d03ff8f3804140
prmacedo <p.ricardomacedo@hotmail.com>
2022-10-03 19:14:05 -0300

    refactor toggle display options

--- assets/scripts/Select.js
+++ assets/scripts/Select.js
@@ -4,14 +4,14 @@ class Select {
 
   constructor() {
     this.#weekday = {
-      select: document.querySelector('.js-weekdaySelect'),
+      select: document.querySelector('.js-weekdaySelect .js-selectField'),
       input: document.querySelector('.js-formWeekday'),
       text: document.querySelector('.js-weekdaySelect .js-selectText'),
       options: document.querySelectorAll('.js-weekdaySelect .js-selectItem')
     }
 
     this.#time = {
-      select: document.querySelector('.js-timeSelect'),
+      select: document.querySelector('.js-timeSelect .js-selectField'),
       input: document.querySelector('.js-formTime'),
       hours: document.querySelector('.js-timeSelect .js-selectText'),
       minutes: document.querySelector('.js-timeSelect .js-selectInput'),
@@ -19,47 +19,69 @@ class Select {
     }
 
     this.#addClickEvent();
+    this.#addChangeEvent();
   }
 
   #addClickEvent() {
-    this.#weekday.select.addEventListener("click", this.#handleWeekdaySelectFieldClick);
+    this.#weekday.select.addEventListener("click", this.#handleClickWeekdaySelect);
     this.#weekday.options.forEach(weekdayItem => {
-      weekdayItem.addEventListener("click", this.#handleWeekdaySelectValue);
+      weekdayItem.addEventListener("click", this.#setWeekdaySelectValue);
     });
 
-    this.#time.select.addEventListener("click", this.#handleTimeSelectFieldClick);
+    this.#time.select.addEventListener("click", this.#handleClickTimeSelect);
     this.#time.options.forEach(timeItem => {
-      timeItem.addEventListener("click", this.#handleTimeSelectValue);
+      timeItem.addEventListener("click", this.#setTimeSelectValue);
     });
   }
 
-  #handleWeekdaySelectFieldClick() {
+  #addChangeEvent() {
+    this.#time.minutes.addEventListener("keyup", this.#maskMinutes)
+  }
+
+  #handleClickWeekdaySelect = () => {
+    this.#toggleWeekdayOptions();
+    this.#removeOptions('.js-timeSelect');
+  }
+
+  #toggleWeekdayOptions() {
     const options = document.querySelector('.js-weekdaySelect .js-selectOption');
     options.classList.toggle('select__options--hidden');
   }
 
-  #handleWeekdaySelectValue = (evt) => {
+  #setWeekdaySelectValue = (evt) => {
     const value = evt.target.getAttribute('data-day');
     const label = evt.target.textContent;
 
     this.#weekday.input.value = value;
     this.#weekday.text.textContent = label;
+
+    this.#toggleWeekdayOptions();
   }
 
-  #handleTimeSelectFieldClick() {
+  #handleClickTimeSelect = () => {
+    this.#toggleTimeOptions();
+    this.#removeOptions('.js-weekdaySelect');
+  }
+
+  #toggleTimeOptions() {
     const options = document.querySelector('.js-timeSelect .js-selectOption');
     options.classList.toggle('select__options--hidden');
   }
 
-  #handleTimeSelectValue = (evt) => {
+  #setTimeSelectValue = (evt) => {
     const label = evt.target.textContent;
 
     this.#time.hours.textContent = label;
     this.#time.input.value = label + "00m"
 
-    this.#time.minutes.addEventListener("keyup", this.#maskMinutes);
     this.#time.minutes.setSelectionRange(2,2);
     this.#time.minutes.focus();
+
+    this.#toggleTimeOptions();
+  }
+
+  #removeOptions(className) {
+    document.querySelector(`${className} .js-selectOption`).classList.add('select__options--hidden');
   }
 
   #maskMinutes = (evt) => {
